9774d8536d8e9af00ad59d2b1d6d69bdfec581b5
Hitesh Yadav <hiteshyadav124001@gmail.com>
2024-09-21 21:47:43 -1200

    add popup

--- script.js
+++ script.js
@@ -441,6 +441,7 @@ function showPopupform() {
 
 // Check if the popup has been shown before
 window.addEventListener("DOMContentLoaded", () => {
+  showPopupform();
   const popupShown = localStorage.getItem("popupShown");
   if (!popupShown) {
     showPopup();
