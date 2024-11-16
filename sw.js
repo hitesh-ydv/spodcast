75be730f550361d5376fa5ba5053d690bbfd38b1
Hitesh Yadav <167308267+hitesh-ydv@users.noreply.github.com>
2024-11-16 19:53:18 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+// sw.js (Service Worker)
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/app.css',
+          '/script.js',
+          '/icons/logo.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
